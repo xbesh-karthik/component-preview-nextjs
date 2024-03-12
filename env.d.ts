f5006f32da28d128a33de6aff50d5e91d851a107
xbesh-karthik <157361582+xbesh-karthik@users.noreply.github.com>
2024-03-12 19:29:51 +0530

    Add files via upload

---
+++ env.d.ts
@@ -0,0 +1,47 @@
+declare module '*.module.less' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.gitignore' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.html' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.md' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.html?raw' {
+
+  export default classes;
+}
+
+declare module '*.ts?worker&inline' {
+
+  export default classes;
+}
+declare module '*.json?raw' {
+
+  export default classes;
+}
+declare module '*.css?raw' {
+
+  export default classes;
+}
+declare module '*.tsx?raw' {
+
+  export default classes;
+}
+
+declare module '*.js?raw' {
+
+  export default classes;
+}
+
